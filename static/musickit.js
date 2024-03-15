2f92bc50a078aefb18854df653d3a8b1144286f1
micahjthompson1 <60897499+micahjthompson1@users.noreply.github.com>
2024-03-15 21:56:38 +0000

    updated dev_token variable

--- static/musickit.js
+++ static/musickit.js
@@ -1,35 +1,33 @@
-const mySecret = process.env['apple_dev_token']
-
-// Configure MusicKit
-MusicKit.configure({
-  developerToken: mySecret,
-  app: {
-    name: 'WordNab',
-    build: '2024.1.00.00',
-  },
-});
-
-// Authorize and Fetch Data
-async function fetchRecentlyPlayed() {
-  const music = MusicKit.getInstance();
-  await music.authorize();
-  const { data: result } = await music.api.music('v1/me/recent/played/tracks');
-
-  // Get the container element where you want to display the data
-  const container = document.getElementById('recently-played');
-
-  // Clear previous content
-  container.innerHTML = '';
-
-  // Iterate over the fetched data and create elements for each item
-  result.data.forEach(track => {
-    // Create a new div element for each track
-    const trackDiv = document.createElement('div');
-    trackDiv.textContent = track.id; // Use the appropriate attribute for the track name
-
-    // Append the new div to the container
-    container.appendChild(trackDiv);
+ // Configure MusicKit with the developer token passed from Flask
+  MusicKit.configure({
+    developerToken: "{{ dev_token }}",
+    app: {
+      name: 'WordNab',
+      build: '2024.1.00',
+    },
   });
-}
 
-fetchRecentlyPlayed();
+  // Authorize and Fetch Data
+  async function fetchRecentlyPlayed() {
+    const music = MusicKit.getInstance();
+    await music.authorize();
+    const { data: result } = await music.api.music('v1/me/recent/played/tracks');
+
+    // Get the container element where you want to display the data
+    const container = document.getElementById('recently-played');
+
+    // Clear previous content
+    container.innerHTML = '';
+
+    // Iterate over the fetched data and create elements for each item
+    result.data.forEach(track => {
+      // Create a new div element for each track
+      const trackDiv = document.createElement('div');
+      trackDiv.textContent = id; // Use the appropriate attribute for the track name
+
+      // Append the new div to the container
+      container.appendChild(trackDiv);
+    });
+  }
+
+  fetchRecentlyPlayed();
