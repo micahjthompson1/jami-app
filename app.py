52bf8333a5bd0974eeccc31d0b150cf3dfdefcb6
micahjthompson1 <60897499+micahjthompson1@users.noreply.github.com>
2024-07-06 14:06:47 +0000

    updated app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def index():
     # Get the developer token from environment variables
     dev_token = os.environ.get('apple_dev_token')
     # Pass the developer token to the template
-    return render_template('index (copy jsonv).html', dev_token=dev_token)
+    return render_template('index.html', dev_token=dev_token)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
