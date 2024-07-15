4fcc922a115300f8303315614afe12ee7f2dd075
micahjthompson1 <60897499+micahjthompson1@users.noreply.github.com>
2024-07-15 20:50:33 +0000

    using trackId instead of isrcCodes

--- app.py
+++ app.py
@@ -16,32 +16,45 @@ db = SQLAlchemy(app)
 class Base(db.Model):
     __tablename__ = 'base_w_isrc'
     song_id = db.Column(db.String)
-    isrc = db.Column(db.String)
+    spotify_track_id = db.Column(db.String)
     word = db.Column(db.String)
     count = db.Column(db.Integer)
     id = db.Column(db.Integer, primary_key=True)
 
 @app.route('/')
 def index():
-    dev_token = os.environ.get('apple_dev_token')
-    return render_template('index.html', dev_token=dev_token)
+    return render_template('index.html')
 
 @app.route('/api/words', methods=['POST'])
 def get_words():
-    isrc_codes = request.get_json('isrcCodes', [])
+    try:
+        data = request.get_json()
+        app.logger.info(f"Received data: {data}")
+        if data is None:
+            raise ValueError("No JSON data received")
 
-    # Query the database
-    query = db.session.query(Base.word, db.func.sum(Base.count).label('total_count')).\
-        filter(Base.isrc.in_(isrc_codes)).\
-        group_by(Base.word).\
-        order_by(db.desc('total_count'))
+        track_ids = data.get('trackIds', [])
+        if not track_ids:
+            raise ValueError("No track IDs provided")
 
-    results = query.all()
+        # Query the database
+        query = db.session.query(Base.word, db.func.sum(Base.count).label('total_count')).\
+            filter(Base.spotify_track_id.in_(track_ids)).\
+            group_by(Base.word).\
+            order_by(db.desc('total_count'))
 
-    # Format the results
-    words = [{'word': row.word, 'total_count': int(row.total_count)} for row in results]
+        results = query.all()
 
-    return jsonify(words)
+        # Format the results
+        words = [{'word': row.word, 'total_count': int(row.total_count)} for row in results]
+
+        return jsonify(words)
+    except ValueError as ve:
+        app.logger.error(f"ValueError: {ve}")
+        return jsonify({"error": str(ve)}), 400
+    except Exception as e:
+        app.logger.error(f"Error occurred: {e}")
+        return jsonify({"error": "Internal Server Error"}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
