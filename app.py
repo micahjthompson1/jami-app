8d928140c139b88106e10923824f752a5f7c3e4a
micahjthompson1 <60897499+micahjthompson1@users.noreply.github.com>
2024-07-17 21:01:57 +0000

    run  with CORS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Database configuration with SSL
 base_connection_string = os.environ.get('DB_CONNECTION_STRING')
