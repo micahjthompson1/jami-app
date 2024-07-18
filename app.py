74336b5df8633a93d57388b6af3aeac19d4596db
micahjthompson1 <60897499+micahjthompson1@users.noreply.github.com>
2024-07-18 03:20:59 +0000

    added mysql connector

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_cors import CORS
+from mysql import connector
 
 app = Flask(__name__)
 CORS(app)
