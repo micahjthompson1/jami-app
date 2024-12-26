35fbcd71411d594ef4ebf9c1a5eaae0bbef04a03
micahjthompson1 <60897499+micahjthompson1@users.noreply.github.com>
2024-12-26 23:54:56 +0000

    updated PORT environment var

--- app.py
+++ app.py
@@ -61,5 +61,5 @@ def match_words():
         return jsonify({'error': 'Internal server error'}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('$PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=True)
